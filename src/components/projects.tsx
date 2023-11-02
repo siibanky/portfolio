e8ab48bf0bf042196285f7b8c1eb5c31b8494c95
sii <siibanky24@gmail.com>
2023-11-02 16:34:13 -0500

    add temp paragraph to each project picture

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -3,6 +3,7 @@ import ProjectsHeader from "./header_components/projects_header";
 import Page from "../enums/page";
 import ProjectDisplays from "./project_displays";
 import "../styles/square_project_displays.css";
+import SplitScreen from "./split_screen";
 
 type ProjectGroupProps = {
   projectName: string;
@@ -20,6 +21,18 @@ const Projects: React.FC<ProjectGroupProps> = ({
       <ProjectDisplays />
       <ProjectsHeader handleClick={handleTextClick} />
       <div className="Projects">
+        <p id="TurtleGameDescription">
+          The game Turtle Swim is similar to Flappy Bird, <br />I created this
+          game because .... <br />
+          Turtle Swim uses React, Typescript, ect...
+        </p>
+        <p id="FoodWebDescription">
+          I created this project using... <br />I was inspired by my love for
+          cooking...
+        </p>
+        <p id="ProjectProjectDescription">
+          Another one of my project descriptions goes here
+        </p>
         <div className="ImageBox">{projectName}</div>
         <p className="Description">{projectDescription} </p>
       </div>
